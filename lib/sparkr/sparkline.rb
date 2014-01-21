5ab621d429dc1a84322e36605e0ad512db4388ac
René Föhring <rf@bamaru.de>
2014-01-21 23:05:00 +0100

    Add failswitch to avoid division by zero

--- lib/sparkr/sparkline.rb
+++ lib/sparkr/sparkline.rb
@@ -6,6 +6,7 @@ module Sparkr
     def initialize(_numbers)
       numbers = _numbers.map(&:to_i)
       step_height = (numbers.max - numbers.min) / (TICKS.size - 1).to_f
+      step_height = 1 if step_height == 0
 
       @ticks = numbers.map do |n|
         index = ((n - numbers.min) / step_height).to_i
